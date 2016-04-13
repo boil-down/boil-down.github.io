4a090704da3d4bb72e32a47b1d1b0f88bb5d0056
Jan Bernitt <jan@jbee.se>
2016-04-13 21:15:58 +0200

    defaults for empty data cells in templates

--- boildown.js
+++ boildown.js
@@ -265,6 +265,7 @@ var bd = (function() {
 	}
 
 	function substLink(link, url, text, alt) {
+		// TODO collect in "links" (need doc for that! this?)
 		for (var i = 0; i < LINKS.length; i++) {
 			if (LINKS[i][1].test(url)) {
 				var cls = LINKS[i][0]+(alt ? " bd-part" : "")
@@ -375,8 +376,8 @@ var bd = (function() {
 				line = line.replace("$0", data.length); 
 				for (var v = 0; v < row.length; v++) {
 					var val = row[v] || "";
-					line = line.replace(new RegExp("\\$"+(v+1)+"<<(.+?)>>"), 
-						function (m, c) { return val? c.replace("$$", val) : ""; });
+					line = line.replace(new RegExp("\\$"+(v+1)+"<<(.+?)>>(?:<<(.+?)>>)?"), 
+						function (m, c, e) { return val? c.replace("$$", val) : e?e:""; });
 					line = line.replace("$"+(v+1), val);
 				}
 				lines.push(line);
